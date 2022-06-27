56238c4e122922d2eb2392f1d460974dfdc3cbe4
MiguelNS <MiguelNS101@gmail.com>
2022-06-27 11:09:21 -0300

    create get status

--- Backend/src/routes/router.ts
+++ Backend/src/routes/router.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import TaskController from '../controllers/task.controller';
+import StatusController from '../controllers/status.controller';
 
 const router = Router();
 
 const taskController = new TaskController();
+const statusController = new StatusController();
 
 router.get('/getTasks', taskController.getAll);
+router.get('/getStatus', statusController.getAll);
 
 export default router;
